790db573ef57769583cefd8bd4fdb0dee81a8d42
sillians <ihuomacbasil@gmail.com>
2026-08-24 12:38:31 +0100

    Update LLM optimization article and add mdx components

---
+++ mdx-components.tsx
@@ -0,0 +1,16 @@
+// mdx-components.tsx
+import type { MDXComponents } from 'mdx/types'
+import Image, { type ImageProps } from 'next/image'
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...components,
+    img: (props) => (
+      <Image
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        {...(props as ImageProps)}
+      />
+    ),
+  }
+}
